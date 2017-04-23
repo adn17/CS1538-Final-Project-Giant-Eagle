9e1f6f2f7e8b8fc609be3c15a29eeb36a6fa276f
adn17 <aaron.d.noll@gmail.com>
2017-04-23 16:15:23 -0400

    Base classes

---
+++ cashier.py
@@ -0,0 +1,5 @@
+class Cashier:
+    def __init__(self, env, speed):
+        self.env = env
+        self.speed = speed
+
---
+++ self_checkout.py
@@ -0,0 +1,4 @@
+class Self_Checkout:
+    def __init__(self, env, speed):
+        self.env = env
+     	
