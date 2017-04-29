2f0f0c99b35fc1edcf54766388850de772aa1d34
elicoyle <elic@zoominternet.net>
2017-04-28 20:32:17 -0400

    peep dat Eli 4-28 file

--- Customer.py
+++ Customer.py
@@ -1,62 +1,85 @@
 import simpy
-import numpy
+from scipy import stats
 
 
 class Customer:
-    def __init__(self, env, items, cashiers, self_checks, id_num):
+    def __init__(self, env, items, speed, cashiers, self_checks, id_num):
         self.env = env
         self.items = items
+        self.speed = speed
         self.destination = None     # Will be a string (I think)
         self.Cashiers = cashiers
         self.Self_Checkouts = self_checks
         self.ID = id_num
 
+        self.employee_involved = False
+        self.kiosk_index = -1
         self.timeArrivedAtLine = 0.0
         self.timeBeginCheckout = 0.0
         self.timeFinished = 0.0
 
+        print("CUSTOMER " + str(self.ID) + "\n", str(self.items), str(self.speed), str(len(self.Cashiers)), str(len(self.Self_Checkouts))+"\n")
+
     def decide_destination(self):
-        # TODO Decide Destination Based on number of items
-        None
+        if self.items > 20:
+            self.destination = "cashier"
+        elif self.items < 5:
+            self.destination = "self"
+        else:
+            if stats.binom.rvs(n=1, p=.25) == 1:        # 25% of people use self checkout
+                self.destination = "self"
+            else:
+                self.destination = "cashier"
+
+        if self.destination == "self":
+            if stats.binom.rvs(n=1, p=.4) == 1:         # 40% of self checkouts involve employee intervention
+                self.employee_involved = True
+
+        print("Customer", str(self.ID), "going to", str(self.destination) + "\n")
 
     # NOTE: Requesting the resource for a register is not done here (or will not be done here):
     # Once a resource is obtained, Customer calls this to wait
     def checkout(self):
         if self.destination == "cashier":
-            # TODO Wait for a time (numer of items * cashier speed)
-            None
+            yield self.env.timeout(self.Cashiers[self.kiosk_index].speed * self.items)
         elif self.destination == "self":
-            # TODO Wait for time (number of items * customer self-checkout speed [or however this is done])
-            None
+            yield self.env.timeout(self.speed * self.items)
+            if self.employee_involved:
+                yield self.env.timeout(15)
 
     def run(self):
-        kiosk_index = -1
         if self.destination == "cashier":
             for i in range(len(self.Cashiers)):     # look for an open kiosk...
                 if not self.Cashiers[i].occupied:   # ...if one is found, go there...
-                    kiosk_index = i
+                    self.kiosk_index = i
                     break
             # TODO: Maybe change this to pick shortest line?
-            if kiosk_index < 0:                     # ...if none are open, pick at random
-                kiosk_index = numpy.random.randint(0, len(self.Self_Checkouts)-1)
+            if self.kiosk_index < 0:                     # ...if none are open, pick at random
+                self.kiosk_index = stats.randint.rvs(0, len(self.Self_Checkouts))
 
-            # self.timeArrivedAtLine = self.env.now() (???)
-            with self.Cashiers[kiosk_index].resource.request() as req:
-                # self.timeBeginCheckout = self.env.now()
+            self.timeArrivedAtLine = self.env.now                             # ?????
+            with self.Cashiers[self.kiosk_index].resource.request() as req:
+                self.timeBeginCheckout = self.env.now                         # ?????
+                print("Customer", str(self.ID), "checking out at cashier", str(self.kiosk_index) + "\n")
                 yield self.env.process(self.checkout())
                 yield req
+                self.timeFinished = self.env.now
 
         elif self.destination == "self":
             for i in range(len(self.Self_Checkouts)):
                 if not self.Self_Checkouts[i].occupied:
-                    kiosk_index = i
+                    self.kiosk_index = i
                     break
             # TODO: Maybe change this to pick shortest line?
-            if kiosk_index < 0:
-                kiosk_index = numpy.random.randint(0, len(self.Self_Checkouts)-1)
+            if self.kiosk_index < 0:
+                self.kiosk_index = stats.randint.rvs(0, len(self.Self_Checkouts))
 
-            # self.timeArrivedAtLine = self.env.now() (???)
-            with self.Cashiers[kiosk_index].resource.request() as req:
-                # self.timeBeginCheckout = self.env.now()
+            self.timeArrivedAtLine = self.env.now                             # ?????
+            with self.Cashiers[self.kiosk_index].resource.request() as req:
+                self.timeBeginCheckout = self.env.now                         # ?????
+                print("Customer", str(self.ID), "checking out at self-check", str(self.kiosk_index)+ "\n")
                 yield self.env.process(self.checkout())
                 yield req
+            self.timeFinished = self.env.now
+
+        print("Customer", self.ID, "Start:" + str(self.timeBeginCheckout) + "End:" + str(self.timeFinished))
--- Sim.py
+++ Sim.py
@@ -1,26 +1,28 @@
-import simpy, numpy
+import simpy
+from scipy import stats
 import Customer, cashier, self_checkout
 
 
 def generate_customers(env):
     customer_id = 0
-    while Env.now() < sim_seconds:
-        customer_items = 0      # TODO Change to random draw based on data
-        new_customer = Customer.Customer(Env, customer_items, Cashiers, Self_Checkouts, customer_id)
+    while Env.now < sim_seconds:
+        customer_items = 10      # TODO Change to random draw based on data
+        customer_speed = 5      # TODO Change to random draw based on data
+        new_customer = Customer.Customer(Env, customer_items, customer_speed, Cashiers, Self_Checkouts, customer_id)
         new_customer.decide_destination()
         env.process(new_customer.run())
         customer_id += 1
         yield Env.timeout(20)   # TODO Change to random draw based on data
 
 
-#   -Number of Cashiers
 # TODO Parse arguments from command line:
+#   -Number of Cashiers
 #   -Number of Self-Checkouts
 #   -How Long to run for??? Unless it just runs for a whole day (this will likely be in hours)
 
-num_cashiers = 0
-num_self_checkouts = 0
-sim_hrs = 0
+num_cashiers = 5
+num_self_checkouts = 5
+sim_hrs = 3
 sim_seconds = sim_hrs * 3600
 
 Cashiers = []
@@ -29,13 +31,13 @@ Self_Checkouts = []
 Env = simpy.Environment()
 # Create the Environment
 for i in range(num_cashiers):
-    Cashiers[i] = cashier.Cashier(env=Env, speed=0)
+    Cashiers.append(cashier.Cashier(env=Env, speed=2))
 
 
 # Generate Resource Objects
 
 for i in range(num_self_checkouts):
-    Self_Checkouts[i] = self_checkout.Self_Checkout(env=Env)
+    Self_Checkouts.append(self_checkout.Self_Checkout(env=Env))
 
 # Prepare the Generator
 Env.process(generate_customers(Env))
